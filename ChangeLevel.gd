6350208757554e18c76e1ad550de2f03af9cd79a
Doctorjellybeans <vicente.rosales.m@mail.pucv.cl>
2025-09-28 19:15:49 -0300

    GameController fix1

---
+++ ChangeLevel.gd
@@ -0,0 +1,35 @@
+class_name GameController extends Node
+
+@export var world_2d: Node2D
+@export var gui: Control
+
+var current_gui_scene
+var current_2d_scene
+
+func _ready() -> void:
+	Global.game_controller = self
+	current_gui_scene = $LevelCompletedScreen
+
+func change_gui_scene(new_scene: String, delete: bool = true, keep_running: bool = false) -> void:
+	if current_gui_scene != null:
+		if delete:
+			current_gui_scene.queue_free()
+		elif keep_running:
+			current_gui_scene.visible = false
+		else:
+			gui.remove_child(current_gui_scene)
+		var new = load(new_scene).instantiete()
+		gui.add_child(new)
+		current_gui_scene = new
+
+func change_2d_scene(new_scene: String, delete: bool = true, keep_running: bool = false) -> void:
+	if current_2d_scene != null:
+		if delete:
+			current_2d_scene.queue_free()
+		elif keep_running:
+			current_2d_scene.visible = false
+		else:
+			gui.remove_child(current_2d_scene)
+		var new = load(new_scene).instantiete()
+		world_2d.add_child(new)
+		current_2d_scene = new
--- Interfaces/LevelCompleted/level_completed_screen.gd
+++ Interfaces/LevelCompleted/level_completed_screen.gd
@@ -1,9 +1,17 @@
 extends Control
 
+var levels = [
+	"res://Levels/tutorial.tscn",
+	"res://Levels/tutorial2.tscn",
+	"res://Levels/lvl1.tscn",
+	"res://Levels/lvl2.tscn",
+	"res://Levels/lvl3.tscn"
+]
+
+var actual_level = 0
 
 func _on_siguiente_nivel_pressed():
-	# Por el momento solo pasara al nivel 2
-	get_tree().change_scene_to_file("res://Levels/lvl2.tscn")
+	Global.game_controller.change_scene_to_packed("")
 
 
 func _on_volver_menu_pressed():
