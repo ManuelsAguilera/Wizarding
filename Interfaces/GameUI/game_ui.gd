ea5837a468db7f3ca64f9f36b0f6adb4684b29cb
manuelsaguilera <manuel.aguilera.devs@gmail.com>
2025-09-29 13:28:31 -0300

    Botones para pasar y resetear el nivel

---
+++ Interfaces/GameUI/game_ui.gd
@@ -0,0 +1,12 @@
+extends Control
+
+
+
+func _on_reset_pressed():
+	Global.game_controller.change_to_level(Global.game_controller.getLevel(Global.level_index))
+	Global.game_controller.change_gui_scene(Global.game_controller.menus["GameUI"], false, true)
+
+
+func _on_next_level_pressed() -> void:
+	Global.game_controller.hide_level()
+	Global.game_controller.change_gui_scene(Global.game_controller.menus["LevelCompleted"], false, true)
